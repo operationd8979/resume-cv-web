4848f1204e51cf74e9c2ce03b4d6bdeabad45103
operationd8979 <operationddd@gmail.com>
2024-03-01 22:17:15 +0700

    update dir src, add structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Container, Grid } from '@material-ui/core';
 import './App.css';
+import Profile from './components/Profile';
+import Header from './components/Header';
+import Portfolio from './pages/Portfolio';
+import Footer from './components/Footer';
 
 function App() {
-    return <div className="App">Hello</div>;
+    return (
+        <Container>
+            <Grid container>
+                <Grid item xs={12} sm={12} md={4} lg={3} style={{ backgroundColor: 'red' }}>
+                    <Profile />
+                </Grid>
+                <Grid item xs style={{ backgroundColor: 'blue' }}>
+                    <Header />
+                    <Portfolio />
+                    <Footer />
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
 
 export default App;
